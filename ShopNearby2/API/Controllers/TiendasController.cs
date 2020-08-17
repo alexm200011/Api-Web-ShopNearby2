388d7b06a567173d73f601cc8c57d3e812503dac
Alex Patricio Montalvo <alexpat123@hotmail.com>
2020-08-16 22:39:14 -0500

    Commit de la Web Api Shop Nearby

---
+++ ShopNearby2/API/Controllers/TiendasController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BEUShopNearby2;
+using BEUShopNearby2.Transactions;
+
+namespace API.Controllers
+{
+    public class TiendasController : ApiController
+    {
+        public IHttpActionResult Post(Tienda Tienda)
+        {
+            try
+            {
+                TiendaBLL.Create(Tienda);
+                return Content(HttpStatusCode.Created, "Tienda creado correctamente");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                List<Tienda> todos = TiendaBLL.List();
+                return Content(HttpStatusCode.OK, todos);
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public IHttpActionResult Put(Tienda Tienda)
+        {
+            try
+            {
+                TiendaBLL.Update(Tienda);
+                return Content(HttpStatusCode.OK, "Tienda actualizado correctamente");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Tienda result = TiendaBLL.Get(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Content(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                TiendaBLL.Delete(id);
+                return Ok("Tienda eliminado correctamente");
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+    }
+}
---
+++ ShopNearby2/BEUShopNearby2/Transactions/TiendaBLL.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUShopNearby2.Transactions
+{
+    public class TiendaBLL
+    {
+        public static void Create(Tienda a)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Tienda.Add(a);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static Tienda Get(int? id)
+        {
+            Entities db = new Entities();
+            return db.Tienda.Find(id);
+        }
+
+        public static void Update(Tienda Tienda)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Tienda.Attach(Tienda);
+                        db.Entry(Tienda).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Tienda Tienda = db.Tienda.Find(id);
+                        db.Entry(Tienda).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+        public static List<Tienda> List()
+        {
+            Entities db = new Entities();
+            return db.Tienda.ToList();
+        }
+    }
+}
+
